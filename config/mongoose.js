44a07cef8e24c538fb7e994750f54d77cf840185
yash12113 <131582248+yash12113@users.noreply.github.com>
2023-08-24 21:01:21 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose'); //requiring mongoose
-mongoose.connect ( 'mongodb://127.0.0.1:27017/hospital_api_development'); //connecting to mongodb
+mongoose.connect ( 'mongodb+srv://yashgadhave8:yash123@cluster0.vjyqqna.mongodb.net/hospitalApi'); //connecting to mongodb
 const db = mongoose.connection; //acquiring the connection
 db.on('error', console.error.bind(console, 'Error in connecting to MongoDB!')); //if error occured
 db.once('open', function() {
